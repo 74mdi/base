12d7cd3cc85e0f7635479c90cbf1814798355020
7amdi <7amdi@tuta.io>
2026-03-12 17:01:24 +0000

    fix

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,10 @@
 import type { ElementType, TableHTMLAttributes } from 'react'
 import type { MDXComponents } from 'mdx/types'
 
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  const Table = (components.table ?? 'table') as ElementType<
+export function useMDXComponents(components: MDXComponents = {}): MDXComponents {
+  const Table = ((components?.table ?? 'table') as ElementType<
     TableHTMLAttributes<HTMLTableElement>
-  >
+  >)
 
   return {
     ...components,
